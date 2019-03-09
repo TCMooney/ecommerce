bca057b103970bcca5410676902145fa5d236228
TCMooney <tcmooney88@gmail.com>
2019-03-09 13:04:30 -0500

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,91 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                total: 19.40,
+                orderNumber: 'A0078778234',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '123 Fake St.',
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40
+                total: 1.30,
+                orderNumber: 'B23623456',
+                orderDate: new Date(),
+                creditCard: '-1111',
+                user: {
+                    name: 'Stink Finker',
+                    shippingAddress: '123 Real St.',
+                }
+            },
+            {
+                _id: 2,
+                total: 20.00,
+                orderNumber: 'C8937678923',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Shawnda Shim',
+                    shippingAddress: '123 Dirt St.',
+                }
+            },
+            {
+                _id: 3,
+                total: 3.50,
+                orderNumber: 'G235623622666',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'JoHudgensrdan ',
+                    shippingAddress: '123 Lamp St.',
+                }
+            },
+            {
+                _id: 4,
+                total: 6.66,
+                orderNumber: 'F2345234666',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jorgin Huggins',
+                    shippingAddress: '123 Phone St.',
+                }
+            },
+            {
+                _id: 5,
+                total: 2.50,
+                orderNumber: 'R2345232323',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jiggins Horgun',
+                    shippingAddress: '123 Cup St.',
+                }
+            },
+            {
+                _id: 6,
+                total: 8.09,
+                orderNumber: 'C8675309666',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '123 Head St.',
+                }
+            },
+            {
+                _id: 7,
+                total: 22.22,
+                orderNumber: 'T342534562323',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Judgen Hriggins',
+                    shippingAddress: '123 Fake St.',
+                }
             }
         ]
     })
