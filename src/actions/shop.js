4e1f4b176e060adeaca9bbbe9b7c10becd99a0a9
TCMooney <tcmooney88@gmail.com>
2019-03-14 20:27:32 -0400

    fetch products action

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,6 +1,7 @@
 import {
     // SET_SHOP_CATEGORIES
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    SET_SHOP_PRODUCTS
 } from "./types";
 
 export function fetchShopCategories() {
@@ -34,6 +35,62 @@ export function fetchShopCategories() {
             {
                 _id: 6,
                 title: 'Ruby'
+            }
+        ]
+    })
+}
+
+export function fetchShopProducts() {
+    return({
+        type: SET_SHOP_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                title: 'JavaScript in the Browser',
+                description: 'PULP [pulp] n. 1. A soft, moist, shapeless mass or matter. 2. A magazine or book containing lurid subject matter and being characteristically printed on rough, unfinished paper.',
+                price: '1.99',
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 1,
+                title: 'Graph Database',
+                description: 'American Heritage Dictionary: New College Edition',
+                price: '',
+                belongsTo: [0, 6]
+            },
+            {
+                _id: 2,
+                title: 'Full Stack Development',
+                description: 'INT. COFFEE SHOP – MORNING',
+                price: '',
+                belongsTo: [0, 1, 4]
+            },
+            {
+                _id: 3,
+                title: 'Javascript Design',
+                description: "A normal Denny's, Spires-like coffee shop in Los Angeles. It's about 9:00 in the morning. While the place isn't jammed, there's a healthy number of people drinking coffee, munching on bacon and eating eggs.",
+                price: '',
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 4,
+                title: 'User Interface Design',
+                description: "Two of these people are a YOUNG MAN and a YOUNG WOMAN. The Young Man has a slight working-class English accent and, like his fellow countryman, smokes cigarettes like they're going out of style.",
+                price: '',
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 5,
+                title: 'User Experience Design',
+                description: 'It is impossible to tell where the Young Woman is from or how old she is; everything she does contradicts something she did. The boy and girl sit in a booth. Their dialogue is to be said in a rapid pace "HIS GIRL FRIDAY" fashion.',
+                price: '',
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 6,
+                title: 'Advanced Object Oriented Programming',
+                description: "No, forget it, it's too risky. I'm through doin' that shit.",price: '',
+                belongsTo: [0, 6]
             }
         ]
     })
